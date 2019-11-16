12bf817d3da9db5314c079629c1cdd8e71ce8ee7
217861 <217861@edu.p.lodz.pl>
2019-11-16 14:34:47 +0100

    Deleted unused commented part of code

--- src/components/buttonComment.jsx
+++ src/components/buttonComment.jsx
@@ -1,39 +1,17 @@
-// import React from "react";
-// import { colors } from "../assets/colors";
-
-// const buttonCommentStyle = {
-//   backgroundColor: colors.AWS_greyBlue,
-//   // backgroundColor: 'white',
-//   borderRadius: "25px",
-//   //border: '2px solid #232f3e';
-//   boxShadow: "2px 2px 2px 2px #232f3e"
-// };
-// function ButtonComment() {
-//   return (
-//     <button
-//       style={buttonCommentStyle}
-//       onClick={() => alert("You commented on the post")}
-//     >
-//       Comment
-//     </button>
-//   );
-// }
-
-// export default ButtonComment;
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import AddCommentIcon from '@material-ui/icons/AddComment';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import AddCommentIcon from "@material-ui/icons/AddComment";
 
 const useStyles = makeStyles(theme => ({
   fab: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
   extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
+    marginRight: theme.spacing(1)
+  }
 }));
 
 export default function FloatingActionButtons() {
@@ -41,8 +19,8 @@ export default function FloatingActionButtons() {
 
   return (
     <div>
-      <Fab color="primary" aria-label="add" className={classes.fab}>  
-        <AddCommentIcon onClick={() => alert("You commented on the post")}/>
+      <Fab color="primary" aria-label="add" className={classes.fab}>
+        <AddCommentIcon onClick={() => alert("You commented on the post")} />
       </Fab>
     </div>
   );
--- src/components/buttonLike.jsx
+++ src/components/buttonLike.jsx
@@ -1,36 +1,16 @@
-// import React from "react";
-// import { colors } from "../assets/colors";
-
-// const buttonLikeStyle = {
-//   margin: "10px",
-//   backgroundColor: colors.AWS_greyBlue,
-//   // backgroundColor: 'white',
-//   borderRadius: "25px",
-//   //border: '2px solid #232f3e';
-//   boxShadow: "2px 2px 2px 2px #232f3e"
-// };
-// function ButtonLike() {
-//   return (
-//     <button style={buttonLikeStyle} onClick={() => alert("you liked the post")}>
-//       Like
-//     </button>
-//   );
-// }
-
-// export default ButtonLike;
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Fab from '@material-ui/core/Fab';
-import AddIcon from '@material-ui/icons/Add';
-import FavoriteIcon from '@material-ui/icons/Favorite';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 const useStyles = makeStyles(theme => ({
   fab: {
-    margin: theme.spacing(1),
+    margin: theme.spacing(1)
   },
   extendedIcon: {
-    marginRight: theme.spacing(1),
-  },
+    marginRight: theme.spacing(1)
+  }
 }));
 
 export default function FloatingActionButtons() {
@@ -38,8 +18,8 @@ export default function FloatingActionButtons() {
 
   return (
     <div>
-      <Fab color="secondary" aria-label="add" className={classes.fab}>  
-        <FavoriteIcon onClick={() => alert("You liked the post")}/>
+      <Fab color="secondary" aria-label="add" className={classes.fab}>
+        <FavoriteIcon onClick={() => alert("You liked the post")} />
       </Fab>
     </div>
   );
