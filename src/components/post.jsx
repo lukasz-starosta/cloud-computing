f6ee4174e485bcc81e877065c8f6cec550d2aab4
Łukasz Starosta <lstarosta98@gmail.com>
2019-11-21 08:25:33 +0100

    NApraweaW

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,67 +1,62 @@
-import React from "react";
-import { colors } from "../assets/colors";
-import Button from "./button";
-import Toolbar from "@material-ui/core/Toolbar";
-import { color } from "@material-ui/system";
+import React from 'react';
+import { colors } from '../assets/colors';
+import Button from './button';
+import Toolbar from '@material-ui/core/Toolbar';
+import { color } from '@material-ui/system';
 
 const image1 = {
   src:
-    "https://i1.wp.com/koomeministries.com/wp-content/uploads/2019/06/profile-placeholder-female.png?fit=250%2C350&ssl=1",
-  alt: "female",
-  width: "162px"
+    'https://i1.wp.com/koomeministries.com/wp-content/uploads/2019/06/profile-placeholder-female.png?fit=250%2C350&ssl=1',
+  alt: 'female',
+  width: '162px'
 };
 
 const image2 = {
   src:
-    "https://149354401.v2.pressablecdn.com/wp-content/uploads/2018/01/placeholder-male-150x150.jpg",
-  alt: "male",
-  width: "162px"
+    'https://149354401.v2.pressablecdn.com/wp-content/uploads/2018/01/placeholder-male-150x150.jpg',
+  alt: 'male',
+  width: '162px'
 };
 const text =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-const name = "Anonim";
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const name = 'Anonim';
 
-const date = "5 minutes ago";
+const date = '5 minutes ago';
 
 const postStyle = {
   backgroundColor: colors.AWS_whiteish,
-  borderRadius: "4px",
-  margin: "40px",
-  paddingLeft: "20px",
-  paddingTop: "3px",
-  paddingBottom: "75px",
-  paddingRight: "20px",
-  boxShadow: "0 10px 30px -10px rgba(0,0,0,0.1)"
+  borderRadius: '4px',
+  margin: '40px',
+  paddingLeft: '20px',
+  paddingTop: '3px',
+  paddingBottom: '75px',
+  paddingRight: '20px',
+  boxShadow: '0 10px 30px -10px rgba(0,0,0,0.1)'
 };
 
 const imageStyle = {
-  textAlign: "center"
+  textAlign: 'center'
 };
 
 const timeAndNameStyle = {
-  lineHeight: "5px",
-  marginBottom: "30px"
+  lineHeight: '5px',
+  marginBottom: '30px'
 };
 
 const outerButtonsStyle = {
-  float: "right"
+  float: 'right'
 };
 
 function Post() {
   return (
     <div style={postStyle}>
-      <p style={timeAndNameStyle}>
+      <div style={timeAndNameStyle}>
         <h3>{name}</h3>
-        <p>{date}</p>
-      </p>
+        <p>{date}</p>\
+      </div>
       <div style={imageStyle}>
         <img src={image1.src} alt={image1.alt} width={image1.width} />
-        <img
-          src={image2.src}
-          alt={image2.alt}
-          width={image2.width}
-          hspace="30"
-        />
+        <img src={image2.src} alt={image2.alt} width={image2.width} hspace="30" />
       </div>
       <p>{text}</p>
       <div>
