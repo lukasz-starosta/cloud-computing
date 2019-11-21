c71979bd993c8632114fc9520dad8aa1a28c246b
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2019-11-21 13:01:55 +0100

    Login view and logic konis

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,147 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import firebase from "firebase";
 
-function Login() {
-  return <>Login</>;
-}
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 20
+  },
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 1,
+    padding: 10,
+    width: "60%",
+    textAlign: "center"
+  },
+  field: {
+    marginBottom: theme.spacing(2)
+  }
+}));
+
+const Login = () => {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    passwordConfirm: ""
+  });
+
+  const classes = useStyles();
+
+  const handleRegistration = () => {
+    if (data.password === data.passwordConfirm) {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(data.email, data.password)
+        .catch(error => {
+          console.log(error.message);
+        });
+    }
+  };
+
+  const handleLogIn = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(data.email, data.password)
+      .catch(error => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.form}>
+        <h3>Log in</h3>
+        <TextField
+          className={classes.field}
+          required
+          variant="outlined"
+          label="Email"
+          onChange={event => {
+            event.persist();
+            setData(rest => {
+              return { ...rest, email: event.target.value };
+            });
+          }}
+        />
+        <TextField
+          className={classes.field}
+          required
+          variant="outlined"
+          label="Password"
+          type="password"
+          onChange={event => {
+            event.persist();
+            setData(rest => {
+              return { ...rest, password: event.target.value };
+            });
+          }}
+        />
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={handleLogIn}
+        >
+          Log me in brotha
+        </Button>
+      </div>
+      <div className={classes.form}>
+        <h3>Create a new account</h3>
+        <TextField
+          className={classes.field}
+          required
+          variant="outlined"
+          label="Email"
+          onChange={event => {
+            event.persist();
+            setData(rest => {
+              return { ...rest, email: event.target.value };
+            });
+          }}
+        />
+        <TextField
+          className={classes.field}
+          required
+          variant="outlined"
+          label="Password"
+          type="password"
+          onChange={event => {
+            event.persist();
+            setData(rest => {
+              return { ...rest, password: event.target.value };
+            });
+          }}
+        />
+        <TextField
+          className={classes.field}
+          required
+          variant="outlined"
+          label="Password Confirmation"
+          type="password"
+          onChange={event => {
+            event.persist();
+            setData(rest => {
+              return { ...rest, passwordConfirm: event.target.value };
+            });
+          }}
+        />
+        <Button
+          variant="contained"
+          color="secondary"
+          size="large"
+          onClick={handleRegistration}
+        >
+          Register me sista
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
