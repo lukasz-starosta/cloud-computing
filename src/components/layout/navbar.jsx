c9f26ad363a3f848bd388b69f146f28a08239a45
Łukasz Starosta <lstarosta98@gmail.com>
2019-12-02 21:57:32 +0100

    siema extreme coding w 45 minut

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,28 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
-import { colors } from "../../assets/colors";
-import firebase from "firebase";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Toolbar, makeStyles } from '@material-ui/core';
+import { colors } from '../../assets/colors';
+import firebase from 'firebase';
+import UserSearch from '../user-search';
 
 const useStyles = makeStyles({
   appBar: {
     backgroundColor: colors.AWS_blue,
-    display: "flex",
-    alignItems: "center"
+    display: 'flex',
+    alignItems: 'center'
   },
   link: {
-    color: "inherit",
-    textDecoration: "none",
-    margin: "0 12px"
+    color: 'inherit',
+    textDecoration: 'none',
+    margin: '0 12px'
   },
   logOut: {
-    background: "transparent",
-    outline: "none",
-    border: "none",
-    color: "white",
+    background: 'transparent',
+    outline: 'none',
+    border: 'none',
+    color: 'white',
     fontSize: 16,
-    fontFamily: "inherit",
+    fontFamily: 'inherit',
     padding: 0
+  },
+  toolbar: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center'
   }
 });
 
@@ -31,7 +37,7 @@ function Navbar({ isLoggedIn }) {
 
   return (
     <AppBar className={classes.appBar}>
-      <Toolbar variant="dense">
+      <Toolbar className={classes.toolbar} variant="dense">
         <Link className={classes.link} to="/dashboard">
           Dashboard
         </Link>
@@ -60,6 +66,7 @@ function Navbar({ isLoggedIn }) {
             </Link>
           </button>
         )}
+        <UserSearch />
       </Toolbar>
     </AppBar>
   );
