92d4c8fbb074d7db7bbddcc4bee2b8f10a2d3e83
karczewskipiotr <34404855+karczewskiPiotr@users.noreply.github.com>
2019-12-07 19:06:17 +0100

    storage wuja

---
+++ src/api/storage.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase';
+
+const storage = {
+  initialize(storage) {
+    this.storageRef = storage.ref();
+  },
+
+  async upload(file) {
+    const ref = this.storageRef.child(file.name);
+    var downloadURL;
+
+    await ref.put(file).then(async snapshot => {
+      await snapshot.ref.getDownloadURL().then(url => {
+        downloadURL = url;
+      });
+    });
+
+    return downloadURL;
+  }
+};
+
+export default storage;
