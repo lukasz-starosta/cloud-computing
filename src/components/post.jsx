41947aa408d2883a41fe5795435c64c57d92f7d4
KrzysztofMoszczynski <Luxtorpeda0104@gmail.com>
2019-12-09 10:59:10 +0100

    Comment section and useStyles

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { colors } from '../assets/colors';
+import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import FloatingActionButton from './floating-action-button';
+import TextField from '@material-ui/core/TextField';
 
 const image1 = {
   src:
@@ -17,7 +19,8 @@ const image2 = {
   width: '162px'
 };
 
-const postStyle = {
+{
+  /*const postStyle = {
   backgroundColor: colors.AWS_whiteish,
   borderRadius: '4px',
   margin: '40px',
@@ -40,29 +43,78 @@ const timeAndNameStyle = {
 const outerButtonsStyle = {
   float: 'right',
   paddingRight: 0
-};
+};*/
+}
+
+const useStyles = makeStyles(theme => ({
+  postStyle: {
+    backgroundColor: colors.AWS_whiteish,
+    borderRadius: '4px',
+    margin: '40px',
+    paddingLeft: '20px',
+    paddingTop: '3px',
+    paddingBottom: '75px',
+    paddingRight: '20px',
+    boxShadow: '0 10px 30px -10px rgba(0,0,0,0.1)'
+  },
+  imageStyle: {
+    textAlign: 'center'
+  },
+  timeAndNameStyle: {
+    lineHeight: '5px',
+    marginBottom: '30px'
+  },
+  outerButtonStyle: {
+    float: 'right',
+    paddingRight: 0
+  },
+  comment: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 600
+    }
+  }
+}));
 
 function Post(props) {
   const { username, post } = props;
-  const { content, created_at } = post;
+  const { content, created_at, likes } = post;
+  const classes = useStyles();
 
   return (
-    <div style={postStyle}>
-      <div style={timeAndNameStyle}>
+    <div className={classes.postStyle}>
+      <div className={classes.timeAndNameStyle}>
         <h3>{username}</h3>
         {/* epoch * 1000 to properly convert to date */}
         <p>{new Date(created_at.seconds * 1000).toUTCString()}</p>
       </div>
-      <div style={imageStyle}>
+      <div className={classes.imageStyle}>
         <img src={image1.src} alt={image1.alt} width={image1.width} />
-        <img src={image2.src} alt={image2.alt} width={image2.width} hspace="30" />
+        <img
+          src={image2.src}
+          alt={image2.alt}
+          width={image2.width}
+          hspace="30"
+        />
       </div>
       <p>{content}</p>
       <div>
-        <Toolbar style={outerButtonsStyle}>
+        <form className={classes.comment} noValidate autoComplete="off">
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Write a comment"
+            multiline
+            rowsMax="3"
+            variant="outlined"
+          />
+        </form>
+      </div>
+      <div>
+        <Toolbar className={classes.outerButtonsStyle}>
           <FloatingActionButton isLikeIcon color="secondary" />
           <FloatingActionButton color="primary" />
         </Toolbar>
+        {likes}
       </div>
     </div>
   );
